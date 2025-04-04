267227bdcdf4b5385feb8487ae8602ef04fa3e9e
davideerusso <rd.96.rd@gmail.com>
2025-04-04 16:08:26 +0200

    mail

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
-const numberFourth = prompt("dammi un numero di 4 cifre");
-let somma = 0;
-// if (numberFourth > 999 && !isNaN(numberFourth)) {
-// }
-for (let i = 0; i < numberFourth.length; i++) {
-  const currentnumber = parseInt(numberFourth[i]);
-  somma += currentnumber;
+//Mail;
+// Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo,
+// utilizzando un ciclo for.
+// Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+// Si può fare? Certo che si basta ragionare un po’.
+//  Nota: Non è necessario provvedere alla validazione delle email
 
-  console.log(currentnumber);
-  console.log(somma);
+const listmail = [
+  "federica@mail.it",
+  "andrea@mail.it",
+  "francesco@mail.it",
+  "luca@mail.it",
+  "miamail@mail.it",
+];
+console.log(listmail);
+let checkmail = false;
+
+const usermail = prompt("scrivi la tua mail", "miamail@mail.it");
+for (let i = 0; i <= listmail.length; i++) {
+  listmail[i] === "miamail@mail.it";
+  checkmail = true;
 }
 
-Mail;
-// Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for. Non è consentito usare nessun metodo proprio degli array (come includes, per esempio). Si può fare? Certo che si basta ragionare un po’. Nota: Non è necessario provvedere alla validazione delle email
+console.log(checkmail);
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
